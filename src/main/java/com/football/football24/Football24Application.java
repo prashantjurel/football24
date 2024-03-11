f88066286d6d9e25bff90eccd78e8389c54bc404
prashantjurel <prashantjurel@outlook.com>
2024-03-11 18:47:59 +0530

    project initialize

---
+++ src/main/java/com/football/football24/Football24Application.java
@@ -0,0 +1,13 @@
+package com.football.football24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Football24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Football24Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/football/football24/Football24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football.football24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Football24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
